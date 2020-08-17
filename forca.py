98885a55a8efb47e2e584652cc132971425771f3
Emilio Santos <emilio.silva@ibpt.org.br>
2020-08-17 18:51:04 -0300

    Inicio do módulo 2

--- forca.py
+++ forca.py
@@ -4,6 +4,14 @@ def jogar():
     print("***Bem vindo no jogo de Forca!***")
     print("*********************************")
 
+    palavra_secreta = "banana"
+
+    enforco = False
+    acertou = False
+
+    while not enforco and not acertou:
+        print("jogando")
+
     print("Fim do jogo!")
 
 
