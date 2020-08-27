ca6a1bb9e21a35f6c40bc033d8cc6a54d1c0d6cc
Emilio Santos <emilio.silva@ibpt.org.br>
2020-08-26 23:39:33 -0300

    Novas implementações

--- forca.py
+++ forca.py
@@ -1,16 +1,25 @@
 def jogar():
-
     print("*********************************")
     print("***Bem vindo no jogo de Forca!***")
     print("*********************************")
 
     palavra_secreta = "banana"
+    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
 
     enforco = False
     acertou = False
 
     while not enforco and not acertou:
-        print("jogando")
+        chute = input("Qual letra? ")
+        chute = chute.strip()
+
+        index = 0
+        for letra in palavra_secreta:
+            if chute.upper() == letra.upper():
+                letras_acertadas[index] = letra
+            index = index + 1
+
+        print(letras_acertadas)
 
     print("Fim do jogo!")
 
